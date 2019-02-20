e75b43e276d23c3220083a57a444c87784cfae1e
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2019-02-20 22:14:08 +0100

    Replace mocha/chai with jest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,8 @@
-import { expect } from "chai";
-
 import { projectSize } from "./index";
 
 describe("projectSize", () => {
   it("returns a number", () => {
     const result = projectSize();
-    expect(result).to.be.a("number");
+    expect(result).toBe(128);
   });
 });
