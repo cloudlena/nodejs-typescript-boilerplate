72416f4c63b81f21b5fb46d8b13c7d3f84ade648
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-09-19 15:49:26 +0200

    Upgrade typescript-eslint to 1.6

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { projectSize } from "./index";
 
-describe("projectSize", () => {
-  it("returns a number", () => {
+describe("projectSize", (): void => {
+  it("returns a number", (): void => {
     const result = projectSize();
     expect(typeof result).toBe("number");
   });
