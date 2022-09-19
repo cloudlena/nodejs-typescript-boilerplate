b197cb98976471d041cab9b6c953fa6fa2d1581d
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-09-19 15:49:26 +0200

    Fix tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,6 @@ import { projectSize } from "./index";
 describe("projectSize", () => {
   it("returns a number", () => {
     const result = projectSize();
-    expect(result).toBe(128);
+    expect(typeof result).toBe("number");
   });
 });
